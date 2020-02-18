36dd9b8a0979569ecc2d65ffeb78e96d66d3435b
Simon Kardell <simonkardell@gmail.com>
2020-02-18 19:24:36 +0100

    cleanup

--- build.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-docker build --pull --squash \
-  -t mhart/alpine-node:12.16.0 \
-  -t mhart/alpine-node:12.16 \
-  -t mhart/alpine-node:12 \
-  -f extract.dockerfile \
-  .
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-for tag in 12.16.0 12.16 12 slim-12.16.0 slim-12.16 slim-12; do
-  git tag -f $tag
-  git push -f origin $tag
-  docker push mhart/alpine-node:$tag
-done
-
-git push
